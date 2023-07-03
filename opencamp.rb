0716ad8fe1a34645a0f3b2b98ef26a94e43661d6
Kyle Chadha <kyle.chadha@gmail.com>
2023-07-03 15:36:50 -0700

    Version 0.3.1

--- opencamp.rb
+++ opencamp.rb
@@ -1,8 +1,8 @@
 class OpenCamp < Formula
   desc "Simple CLI for interacting with the recreation.gov API"
   homepage "https://github.com/opencamp-hq/cli"
-  url "https://github.com/opencamp-hq/cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/opencamp-hq/cli/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "5c470c2c4eca68d0e78ee9ca9fd5c08b010b1f35a77a7e6494011548954537c6"
   license "MIT"
 
   depends_on "go" => :build
